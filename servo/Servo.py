0aba6e905f338b1df9cc16b3aa8736dd0ec3fc4e
Durian <32596839+Kylamber@users.noreply.github.com>
2022-06-18 17:17:51 +0700

    Update Servo.py

--- servo/Servo.py
+++ servo/Servo.py
@@ -2,7 +2,7 @@ class Servo:
   def __init__(self, servo):
     """
     Args:
-    servo - a PWM class of the servo
+    -> servo: a PWM object of the servo
     """
     
     self.servo = servo
@@ -21,15 +21,14 @@ class Servo:
     - 1.5 ms = 0 deg
     - 2 ms   = 90 deg
     
-    1 ms in 20 ms period is 1/20 of the pulse or 5% of the pulse. Using the duty
-    method in PWM is the ratio of duty/1023. E.g. for a 50% pulse, a duty of 512
-    is used (512/1023 is roughly 50%). For a 5% pulse, that means a duty of 1023/20 
-    and for 2/20 or 10% pulse, that means a duty of 1023*2/20. Generally, it's 
-    1023*t/20 with t being the pulse width. (read more: 
+    The duty method in PWM sets the ratio of the argument to 1023, e.g. PWM.duty(512)
+    is 512/1023 which is roughly 50%. Since 1 ms in 20 ms period is 1/20 or 5% of the
+    period, the duty is (1023 * 1/20). For 2 ms in 20 ms period is 2/20 or 10% of the
+    period, the duty is (1023 * 2/20). (read more: 
     https://docs.micropython.org/en/latest/esp32/quickref.html#pwm-pulse-width-modulation)
     
     Assuming it's linear, the line equation given that -90 corresponds with a duty 
-    of 1023/20 and 90 corresponds with a duty of 1023*2/20, the line equation becomes
+    of 1023/20 and 90 corresponds with a duty of 1023*2/20 is
     duty = 341/1200 * angle + 3069/40
     """
     
